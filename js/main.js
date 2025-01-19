197544b1d789c21f344de3479fac405bff6ec0f8
AlexKhom17 <alexkhomich170290@gmail.com>
2025-01-19 15:37:40 +0300

    изменения portfolio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+       var gallary = document.querySelectorAll('.materialboxed');
+     var instances_gallary = M.Materialbox.init(gallary);
   });
+//     document.addEventListener('DOMContentLoaded', function() {
+//     var gallary = document.querySelectorAll('.materialboxed');
+//      var instances_gallary = M.Materialbox.init(gallary);
+//   });
